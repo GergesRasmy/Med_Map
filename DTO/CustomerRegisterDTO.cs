440bc035d5a9660217293f93c112ee668bc02355
gergesrasmy <gergesrasmy33@gmail.com>
2026-02-23 15:00:45 +0200

    .

--- DTO/CustomerRegisterDTO.cs
+++ DTO/CustomerRegisterDTO.cs
@@ -5,7 +5,7 @@
         [Required]
         [MinLength(3, ErrorMessage = "Minimum length is 3")]
         [MaxLength(30, ErrorMessage = "Maximum length is 30")]
-        public string TraddeName { get; set; }
+        public string UserName { get; set; }
         [Required]
         [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
